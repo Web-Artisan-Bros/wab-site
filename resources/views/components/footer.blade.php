a63f1f513bd2a81cc891dcd0c159d064b8f3c9f6
marius-leica <mariusleica@gmail.com>
2023-01-10 16:22:33 +0100

    Nav Component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,54 +1,59 @@
-<div class="container border pt-5 pb-5">
-    
-
-    <div>
-        <div class="row row-cols-2 g-5 ">
+<div class="container border py-5">
+    <div class="py-md-5">
+        <div class="row py-md-5">
             <div class="col">
-                <div>
-                    <div class="d-flex">
-                    <h3 class="text-primary">PRONTO PER COLLABORARE</h3>
-                    <h1 class="text-primary">?</h1>
+                <div class="mb-5">
+                    <div class="d-flex align-items-center border-bottom border-3 border-primary div-with-title">
+                        <h3 class="">PRONTO PER COLLABORARE</h3>
+                        <h2 class="fs-max">?</h2>
                     </div>
-                    
-                    <hr class="border border-primary border-2 opacity-50">
                 </div>
-                <button class="btn btn-primary">SCRIVICI</button>
-                <p>Copyright 2023 Web Artisan Bros. All Right Reserved</p>
-                <div>
-                    <a href="#" class="me-5 text-light">Privacy Policy</a>
-                    <a href="#" class="text-light">Cookie Policy</a>
+                <div class="d-flex flex-column align-items-start">
+                    <button class="btn btn-primary">
+                        SCRIVICI
+                        <img src="/assets/VectorArrow.svg" alt="">
+                    </button>
+                    <div class="flex-fill">
+                        <p class="pt-5"><small>Copyright 2023 Web Artisan Bros. All Right Reserved</small></p>
+                        <div>
+                            <a href="#" class="me-5 text-light"><small>Privacy Policy</small></a>
+                            <a href="#" class="text-light"><small>Cookie Policy</small></a>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div class="col-lg-2"></div>
             <div class="col">
-                <div>
-                    <div class="d-flex align-items-baseline">
-                        <div>
-                            <img src="/assets/Logo.png" class="img-fluid w-50" alt="logo">
-                        </div>
-                        <h4><span  class="text-primary">W</span>eb <span class="text-primary">A</span>rtisan <span class="text-primary">B</span>ros</h4>
+                <div class="mb-5">
+                    <div class="d-flex align-items-end justify-content-between  border-bottom border-3 border-primary  div-with-title">
+
+                        <img class="me-2" src="/assets/Logo.svg" style="max-width: 170px; max-height: 113px;" alt="logo">
+
+                        <p class="fs-3  mt-auto"><span class="text-primary">W</span>eb <span class="text-primary">A</span>rtisan <span class="text-primary">B</span>ros</p>
+
                     </div>
-                    <hr class="border border-primary border-2 opacity-50">
-                    <div class="d-flex justify-content-between">
-                        <p>Info</p>
-                        <div>
-                            <img src="/assets/VectorMail.png" alt="Mail Icon">
-                            <img src="/assets/VectorInsta.png" alt="Instagram Icon">
-                            <img src="/assets/VectorGit.png" alt="Git Hub Icon">
-                            <img src="/assets/VectorInsta.png" alt="Instagram Icon">
-                        </div>
+                    <!-- <hr class="border border-primary border-2 opacity-100 mb-5"> -->
+                </div>
+                <div class="d-flex justify-content-between mb-2">
+                    <p class="lead">Info:</p>
+                    <div class="align-bottom">
+                        <img src="/assets/VectorMail.svg" alt="Mail Icon" class="me-2">
+                        <img src="/assets/VectorInsta.svg" alt="Instagram Icon" class="me-2">
+                        <img src="/assets/VectorGit.svg" alt="Git Hub Icon" class="me-2">
+                        <img src="/assets/VectorInsta.svg" alt="Instagram Icon">
                     </div>
-                    <p>WEB ARTISAN BROS
-VIA SAN ROCCO, 34/D - 36016 THIENE</p>
-<p>Telefono: 3290241530</p>
-<p>E-mail: info@webartisan.com</p>
-<p>
-P. IVA 04439020241</p>
                 </div>
-                </div>
-            
+                <p>Web Artisan Bros
+                    Via San Rocco, 34/D - 36016 Thiene (VI)</p>
+                <p>Telefono: 3290241530</p>
+                <p>E-mail: Info@webartisan.com</p>
+                <p>
+                    P. IVA 04439020241</p>
+            </div>
         </div>
     </div>
-        
-    
+</div>
+
+
 
 </div>
--- resources/views/components/navlink.blade.php
+++ resources/views/components/navlink.blade.php
@@ -1,11 +1,3 @@
-
-<div>
-
-  questo viene dalla nav
-
-</div>
-
-
 {{-- @props([
   "active" => false,
 ]) --}}
@@ -13,3 +5,49 @@
 {{-- <a class="nav-link {{ $active ? 'active' : ''  }}" href="{{ $href }}">
   {{ $slot }}
 </a> --}}
+<div>
+
+  <nav class="navbar navbar-expand-lg py-3 navbar-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="/assets/NavLogoYW.svg" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">HOME</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            SERVIZI
+            <img src="/assets/VectorArrowWhite.svg" alt="">
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item text-light" href="#">SITI WEB</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">SEO</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">SOCIAL MARKETING</a><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">WEB DESIGN</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">PACHETTO BUSINESS</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CHI SIAMO</a>
+        </li>
+      </ul>
+      <button class="btn btn-primary">
+        SCRIVICI
+      <img src="/assets/VectorArrow.svg" alt="">
+      </button>
+    </div>
+  </div>
+</nav>
+
+</div>
+
+
