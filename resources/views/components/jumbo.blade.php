5a7ad3b994ad1b7e908cefab9b0b811c2f9857ca
marius-leica <mariusleica@gmail.com>
2023-01-11 13:12:14 +0100

    square btn created

--- resources/views/components/jumbo.blade.php
+++ resources/views/components/jumbo.blade.php
@@ -2,17 +2,22 @@
     <div class="container container-p-156">
         <div class="row pb-sm-5">
             <div class="col">
-                <button class="btn btn-primary">Contattaci</button>
+                <div class="ps-3">
+                   <x-squarebtn/> 
+                </div>
+                
+
             </div>
+            <!-- <div class="col-lg-2"></div> -->
             <div class="col">
                 <div class="mb-5">
-                    <h1>WEB DESIGN</h1>
-                    <h2 class="fs-3 f-spacing">PERSONALIZZATO</h2>
-                    <h2>E ARTIGIANALE</h2>
+                    <h1 class="text-end">WEB DESIGN</h1>
+                    <h2 class="fs-3 f-spacing text-end">PERSONALIZZATO</h2>
+                    <h2 class="text-end">E <span class="highlight">ARTIGIANALE</span></h2>
                 </div>
                 <div>
                     <div class="d-flex">
-                        <p class="lead">Aiutiamo il tuo business a farsi notare in un mondo digitale</p>
+                        <p class="lead">Aiutiamo il tuo business a farsi <span class="highlight">notare</span> in un mondo digitale</p>
                         <div class="w-25 ps-2">
                             <hr>
                         </div>
@@ -22,7 +27,8 @@
             </div>
             
         </div>
-        <div class="pt-5">
+        <div class="pt-5 position-relative">
+            <img class="w-100 img-border" src="/assets/JumboHomeBorder.png" alt="">
             <img class="w-100" src="/assets/JumboHomeColor.png" alt="">
         </div>
     </div>
