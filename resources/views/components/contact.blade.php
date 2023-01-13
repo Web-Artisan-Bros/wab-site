bbd00cff0f44c1c68934ba19954b4b87b03fe2e7
marius-leica <mariusleica@gmail.com>
2023-01-13 10:13:56 +0100

    contact section done

---
+++ resources/views/components/contact.blade.php
@@ -0,0 +1,33 @@
+<section class="bg-primary ">
+    <div class="container container-p-156 dark-text">
+        <div class="row">
+            <div class="col">
+                <div class="ps-3">
+                    <x-square-btn arrowImg="/assets/WhiteArrow.svg" textBorderColor="bordeTextBlack" reverse="btn-reverse-white"/>
+                </div>
+
+
+            </div>
+            <!-- <div class="col-lg-2"></div> -->
+            <div class="col">
+                <div class="mb-5">
+
+                    <h2 class="fs-3 f-spacing text-start text-dark">La tua <span class="highlightRed">storia</span> attraverso
+                        un sito che parli di <span class="highlightRed">te</span></h2>
+                    <hr class="border-dark opacity-100 w-75">
+
+                </div>
+                <div>
+                    <div class="d-flex">
+                        <p class="lead text-dark">Qualsiasi cosa per aiutarti a definire la tua presenza online, trasformiamo le tue idee e le tue necessità in un prodotto concreto!</p>
+                        <div class="w-25 ps-2">
+                            <hr>
+                        </div>
+                    </div>
+                    <!-- <p class="lead"></p> -->
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
