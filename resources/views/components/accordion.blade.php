7cb4fd973088c9d481497fe467b0e34527b99c84
marius-leica <mariusleica@gmail.com>
2023-01-13 10:13:08 +0100

    starting accordion section

---
+++ resources/views/components/accordion.blade.php
@@ -0,0 +1,54 @@
+<section class="bg-light text-dark">
+    <div class="container container-p-156">
+        <div class="row align-items-end pb-3">
+            <div class="col-5">
+                <h2><span class="highlight">SCEGLI NOI</span> <span class="text-dark"> SENZA ESITARE</span></h2>
+            </div>
+            <div class="col-2">
+                <hr class="opacity-100 pb-4">
+            </div>
+            <div class="col-5">
+                <p class="lead ">ECCO PERCHE’ SCEGLIERE NOI, E’ UNA <span class="highlight  ">DECISIONE FACILE</span>:</p>
+            </div>
+        </div>
+
+        <div class="accordion accordion-flush mt-5 wab-accordion" id="accordionFlushExample">
+            <div class="accordion-item d-flex">
+                <div class="d-flex">
+                    <img class="" src="/assets/BorderedArrow.svg" alt="">
+                    <div class="py-4">
+                        <p class="lead ps-4 mb-0">01.</p>
+                        <h2 class="accordion-header " id="flush-headingOne">
+                            <button class="accordion-button collapsed pt-0 pb-0" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                ESPERIENZA CONSOLIDATA
+                            </button>
+                        </h2>
+                    </div>
+                </div>
+                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">il nostro team vanta anni di esperienza nel settore e un team altamente qualificato, garantendo risultati di alta qualità.</div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="flush-headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                        METODO ARTIGIANALE
+                    </button>
+                </h2>
+                <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">il nostro team vanta anni di esperienza nel settore e un team altamente qualificato, garantendo risultati di alta qualità.</div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="flush-headingThree">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                        AUMENTO DEI PROFITTI
+                    </button>
+                </h2>
+                <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">il nostro team vanta anni di esperienza nel settore e un team altamente qualificato, garantendo risultati di alta qualità.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
