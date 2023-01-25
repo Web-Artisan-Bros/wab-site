79cc76f23b4d421f0ffb08f8a463532b5fb4cb97
marius-leica <mariusleica@gmail.com>
2023-01-25 18:06:02 +0100

    chi siamo jumbo

--- resources/views/chisiamo.blade.php
+++ resources/views/chisiamo.blade.php
@@ -3,5 +3,40 @@
 @section ('content')
 <x-navlink background="yellow-nav" logo="/assets/NavLogoR.svg" />
 
+<section class="bg-primary">
+ <div class="container container-p-156">
+  <div class="row flex-row-reverse pb-sm-5">
+   <div class="col">
+    <div class="ps-3">
+     <x-square-btn arrowImg="/assets/WhiteArrow.svg" textBorderColor="bordeTextBlack" reverse=" btn-hover justify-content-end" />
+    </div>
+   </div>
+   <div class="col">
+    <div class="mb-5">
+
+     <h1 class=" text-dark">SIAMO <span class="text-dark wab-highlight wab-highlight--secondary">
+       WEB </span><span class="text-dark wab-highlight wab-highlight--secondary">
+       ARTISAN </span><span class="text-dark wab-highlight wab-highlight--secondary">
+       BROS</span></h1>
+    </div>
+    <div>
+     <div class="d-flex flex-row-reverse">
+      <p class="lead text-dark">Aiutiamo il tuo business a farsi <span class="wab-highlight wab-highlight--secondary">notare</span> in un mondo digitale</p>
+      <hr class="w-25 opacity-100 secondary-hr">
+     </div>
+    </div>
+   </div>
+  </div>
+  <div class="pt-5 position-relative">
+   <img class="w-100 img-border" src="/assets/JumboChiSiamoBorder.png" alt="a yellow border of the img">
+   <figure class="h-100">
+    <div class="img-area w-100">
+     <img class="w-100 img-bw" src="/assets/JumboChiSiamoColor.png" alt="Welcome neon sign">
+    </div>
+   </figure>
+  </div>
+ </div>
+</section>
+
 <x-footer background="yellow-footer" />
 @endsection
--- resources/views/components/jumbo.blade.php
+++ resources/views/components/jumbo.blade.php
@@ -3,42 +3,29 @@
     <div class="row pb-sm-5">
       <div class="col">
         <div class="ps-3">
-          <x-square-btn arrowImg="/assets/YellowArrow.svg" textBorderColor="bordeTextWhite" reverse=" btn-hover"/>
-
-
-
+          <x-square-btn arrowImg="/assets/YellowArrow.svg" textBorderColor="bordeTextWhite" reverse=" btn-hover" />
         </div>
       </div>
-
-      <!-- <div class="col-lg-2"></div> -->
       <div class="col">
         <div class="mb-5">
           <h1 class="">WEB DESIGN</h1>
           <h2 class="fs-3 f-spacing ">PERSONALIZZATO</h2>
           <h2 class="">E <span class="text-dark wab-highlight">
-                  {{--  <img class="highlightXXL  position-absolute top-50 start-50 translate-middle"
-                         src="/assets/EvidenziatoXXL.svg" alt="">    --}}
-                    ARTIGIANALE</span></h2>
+              ARTIGIANALE</span></h2>
         </div>
         <div>
           <div class="d-flex">
             <p class="lead">Aiutiamo il tuo business a farsi <span class="wab-highlight text-dark">notare</span> in un mondo digitale</p>
-            <!-- <div class="w-25 ps-2"> -->
             <hr class="w-25 opacity-100">
-            <!-- </div> -->
           </div>
-          <!-- <p class="lead"></p> -->
         </div>
       </div>
-
     </div>
     <div class="pt-5 position-relative">
       <img class="w-100 img-border" src="/assets/JumboHomeBorder.png" alt="a yellow border of the img">
       <figure class="h-100">
         <div class="img-area w-100">
-
           <img class="w-100 img-bw" src="/assets/JumboHomeColor.png" alt="Welcome neon sign">
-
         </div>
       </figure>
     </div>
