ad73b7ac41270ba1e0800d7a34676dfb24f6ead8
leicaflorian <florian.leica@gmail.com>
2023-03-13 18:43:11 +0100

    Updates admin password

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,7 @@ class DatabaseSeeder extends Seeder {
       User::factory()->create([
         'name'     => 'Wab Admin',
         'email'    => 'info@webartisanbros.com',
-        'password' => bcrypt(App::environment('production') ? '@XCluDXTeCJ7g!N' : 'password'),
+        'password' => bcrypt(App::environment('production') ? config("ADMIN_PASSWORD") : 'password'),
       ]);
     }
   }
