8186ec9466d93c9d2abf0ef3ff2facf55cf66fa3
leicaflorian <florian.leica@gmail.com>
2023-04-14 16:05:59 +0200

    Adds svg favicon

--- resources/views/components/head/favicon.blade.php
+++ resources/views/components/head/favicon.blade.php
@@ -1,6 +1,8 @@
 <link rel="apple-touch-icon" sizes="180x180" href="/assets/og/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/assets/og/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/assets/og/favicon-16x16.png">
+<link rel="icon" type="image/png" href="/assets/og/favicon.png">
+<link rel="icon" type="image/svg+xml" href="/assets/og/favicon.svg">
 
 <link rel="manifest" href="/assets/og/site.webmanifest">
 <link rel="mask-icon" href="/assets/og/safari-pinned-tab.svg" color="#1d1d1d">
