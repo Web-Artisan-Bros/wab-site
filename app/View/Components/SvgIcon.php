099dd3582975909da6793432988d0c27075fb81b
leicaflorian <florian.leica@gmail.com>
2023-04-14 17:21:14 +0200

    Handle error in svgIcon component

--- app/View/Components/SvgIcon.php
+++ app/View/Components/SvgIcon.php
@@ -4,30 +4,32 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class SvgIcon extends Component
-{
+class SvgIcon extends Component {
   public $name;
+  
   /**
    * Create a new component instance.
    *
    * @return void
    */
-  public function __construct($icon)
-  {
+  public function __construct($icon) {
     $this->name = $icon;
   }
-
+  
   /**
    * Get the view / contents that represent the component.
    *
    * @return \Illuminate\Contracts\View\View|\Closure|string
    */
-  public function render()
-  {
-    $fileContent = file_get_contents(public_path('assets/' . $this->name . '.svg'));
-
-    return view('components.svg-icon', [
-      'fileContent' => $fileContent
-    ]);
+  public function render() {
+    try {
+      $fileContent = file_get_contents(public_path('assets/' . $this->name . '.svg'));
+      
+      return view('components.svg-icon', [
+        'fileContent' => $fileContent
+      ]);
+    } catch (\Exception $e) {
+      return '';
+    }
   }
 }
