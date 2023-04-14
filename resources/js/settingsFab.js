936375cc086c7f7cf51fab4a3761e75c5de76904
leicaflorian <florian.leica@gmail.com>
2023-04-14 19:19:43 +0200

    Adds settings fab js

---
+++ resources/js/settingsFab.js
@@ -0,0 +1,33 @@
+let settingsContainer
+
+window.addEventListener('DOMContentLoaded', function () {
+  settingsContainer = document.querySelector('.settings-fab-container')
+})
+
+window.addEventListener('pageChanged', function () {
+  addIntersectionObserver()
+})
+
+function addIntersectionObserver () {
+  // recupero le singole sezioni
+  const sections = document.querySelectorAll('main > .themed-section, footer ')
+  
+  // creo un observer
+  const observer = new IntersectionObserver(callback, {
+    rootMargin: '-95% 0% 0% 0%'
+  })
+  
+  // console.log(sections)
+  
+  sections.forEach((section, i) => {
+    observer.observe(section)
+  })
+}
+
+function callback (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && settingsContainer) {
+      settingsContainer.dataset.bsTheme = entry.target.dataset.bsTheme
+    }
+  })
+}
--- resources/views/components/settings-fab.blade.php
+++ resources/views/components/settings-fab.blade.php
@@ -1,13 +1,13 @@
-<div class="settings-fab-container" data-bs-theme="{{$theme}}">
+<div class="settings-fab-container d-none d-lg-block" data-bs-theme="{{$theme}}">
     <div class="btn-group dropup">
         <button type="button" class="btn btn-themed " data-bs-toggle="dropdown" aria-expanded="false"
-                data-bs-auto-close="outside">
+                data-bs-auto-close="outside" title="Language">
             <x-svg-icon icon="icons/lang" class="settings-icon" size="32px"></x-svg-icon>
         </button>
 
-        <div class="dropdown-menu">
-            <div class="list-group list-animate-items list-group-flush py-3 flex-fill">
-                <a class="list-group-item list-group-item-action point-up text-uppercase collapsed"
+        <div class="dropdown-menu border">
+            <div class="list-group list-animate-items list-group-flush py-2 flex-fill">
+                {{--<a class="list-group-item list-group-item-action point-up text-uppercase collapsed"
                    data-bs-toggle="collapse" href="#settings-language-menu" role="button"
                    aria-expanded="false" aria-controls="mobile-services-menu">
                     {{ __("Language")  }}
@@ -15,16 +15,16 @@
                     <x-svg-icon icon="arrow_top_right" size="18px" class="me-2 ms-0" data-active-direction="up"/>
                 </a>
 
-                <div class="collapse" id="settings-language-menu">
-                    <div class="list-group list-group-flush border-bottom">
-                        @foreach(config("app.validLocales") as $lang)
-                            <a href="{{ \App\Providers\GlobalHelperProvider::localizeUrl($lang["code"]) }}"
-                               class="list-group-item list-group-item-action text-uppercase">
-                                {{ $lang['label'] }}
-                            </a>
-                        @endforeach
-                    </div>
-                </div>
+                <div class="collapse" id="settings-language-menu">--}}
+                {{--                    <div class="list-group list-group-flush border-bottom">--}}
+                @foreach(config("app.validLocales") as $lang)
+                    <a href="{{ \App\Providers\GlobalHelperProvider::localizeUrl($lang["code"]) }}"
+                       class="list-group-item list-group-item-action text-uppercase">
+                        {{ $lang['label'] }}
+                    </a>
+                @endforeach
+                {{--                    </div>--}}
+                {{--                </div>--}}
 
             </div>
         </div>
