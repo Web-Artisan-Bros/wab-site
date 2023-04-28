5e527678de96919b1befc43afbf2f7c0172759fe
leicaflorian <florian.leica@gmail.com>
2023-04-28 18:19:38 +0200

    Updates florian qoute

--- resources/views/team/florian.blade.php
+++ resources/views/team/florian.blade.php
@@ -58,8 +58,8 @@ $pageTitle = "Leica Florian Robert";
     <div class="row row-cols-1 row-cols-md-2 gx-5">
       <div class="col">
         <blockquote class="blockquote text-end">
-          <p class="fs-4">"{!! __('Non esiste <span class="wab-highlight">limite</span> se non la propria <span
-              class="wab-highlight">fantasia')!!}</span> !"</p>
+          <p class="fs-4">"{!! __('L\'unico <span class="wab-highlight">limite</span> siamo <span
+              class="wab-highlight">noi stessi')!!}</span> !"</p>
           <p class="blockquote-footer"><cite title="Leica Florian Robert">Leica Florian Robert</cite></p>
         </blockquote>
       </div>
