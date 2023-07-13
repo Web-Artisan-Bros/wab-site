99a947ed180260070b6523457bb2f751a85bfb4e
marius-leica <mariusleica@gmail.com>
2023-07-13 17:57:55 +0200

    removed broken img from web-dev page

--- resources/views/web.blade.php
+++ resources/views/web.blade.php
@@ -105,7 +105,7 @@ $pageTitle = trans('menu_dropdown.Sviluppo Web');
           aria-labelledby="{{ 'acc_head_' . $entry['id'].'0' }}" data-bs-parent="#cardsServices">
           <div class="accordion-body d-flex text-break">
             {{ trans('cards.' . $entry['content'])}}
-            <img class="cup d-none d-lg-block" src="/assets/CoffeeMug.svg" alt="">
+            
           </div>
         </div>
       </div>
